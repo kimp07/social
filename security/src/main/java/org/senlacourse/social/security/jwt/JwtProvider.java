68f2ce68a8c2fb1811e5784a061556e48a11b82f
kimp07 <kimp07@gmail.com>
2021-05-27 10:14:15 +0300

    Files transport, UserImageService.java StorageController.java

--- security/src/main/java/org/senlacourse/social/security/jwt/JwtProvider.java
+++ security/src/main/java/org/senlacourse/social/security/jwt/JwtProvider.java
@@ -21,7 +21,6 @@ import org.springframework.security.authentication.dao.AbstractUserDetailsAuthen
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
