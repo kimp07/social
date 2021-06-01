b73dc8f4942f3fc4c181613fcdec8f0e2cf91bd6
Alex <kimp07@gmail.com>
2021-06-01 18:41:41 +0300

    Debugging Tests

---
+++ src/test/java/org/senlacourse/social/TalksEndToEndTest.java
@@ -0,0 +1,15 @@
+package org.senlacourse.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class TalksEndToEndTest {
+
+    @Test
+    void addNewTalkTest() {
+
+    }
+}
